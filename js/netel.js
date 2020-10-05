e334be2e8c8931f18f978575820b98608190f777
OluwaDAN <danielloluwatosin@gmail.com>
2020-10-05 03:30:27 +0100

    netel

---
+++ js/netel.js
@@ -0,0 +1,7 @@
+let myMenu=document.querySelector(".menu");
+let myBtn=document.querySelector(".btn");
+myBtn.addEventListener("click", show);
+function show() {
+    myMenu.classList.toggle("dan");
+    
+}
